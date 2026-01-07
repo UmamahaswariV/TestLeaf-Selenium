b8aa7bb4d5da5dcdedac7502c699efdd3948a575
UmamahaswariV <136911617+UmamahaswariV@users.noreply.github.com>
2026-01-07 19:24:10 +0530

    HelloTestLeaf.java

---
+++ week1/day1/HelloTestLeaf.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class HelloTestLeaf {
+	/*
+	 * @Classroom 1.0 From Classroom Day 1
+	 */
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Testleaf");
+		}		
+	}
