e322075adc266c98f2bf467912b97e47b87587eb
UmamahaswariV <136911617+UmamahaswariV@users.noreply.github.com>
2026-01-07 19:26:12 +0530

    Create Chrome.java

---
+++ week1/day2/Chrome.java
@@ -0,0 +1,14 @@
+package week1.day2;
+public class Chrome {
+	public static void main(String[] args) {
+		//
+		float browserVersion = 91.0f;
+		String browserName = "Chrome";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		System.out.println(browserVersion+"\n"+browserName+"\n"+isBeta+"\n"
+		+releaseYear+"\n"+shortcutKey);
+	}
+
+}
