a4dfda5c57a6dcb0d9966f614d6d8bf0aa9baa8d
UmamahaswariV <136911617+UmamahaswariV@users.noreply.github.com>
2026-01-28 22:53:01 +0530

    Create JavaConnection.java

---
+++ week3/day2/JavaConnection.java
@@ -0,0 +1,26 @@
+public class JavaConnection implements DatabaseConnection {
+	@Override
+	public void connect() {
+		System.out.println("Connected to Java Database(JDBCDriver Loaded)");
+		
+	}
+
+	@Override
+	public void disconnect() {
+		 System.out.println("Disconnected from Database");
+		
+	}
+
+	@Override
+	public void executeUpdate() {
+		System.out.println("SQL UPDATE executed successfully");
+		
+	}
+	public static void main(String[] args) {
+		JavaConnection db=new JavaConnection();
+		db.connect();
+		db.disconnect();
+		db.executeUpdate();
+	}
+
+}
