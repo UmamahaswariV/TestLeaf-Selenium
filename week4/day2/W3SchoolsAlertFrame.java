a6898e0a398a4fefb9901afe20fd7776fb27152b
UmamahaswariV <136911617+UmamahaswariV@users.noreply.github.com>
2026-01-30 21:27:37 +0530

    W3 Schools Alert Interaction

--- week4/day2/W3SchoolsAlertFrame.java
+++ week4/day2/W3SchoolsAlertFrame.java
@@ -5,7 +5,7 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.chrome.ChromeDriver;
-public class AlertFrame {
+public class W3SchoolsAlertFrame {
 
 	public static void main(String[] args) throws IOException {
 		ChromeDriver driver; 
