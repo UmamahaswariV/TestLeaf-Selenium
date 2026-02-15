ded9cdb9c61326dedc324342e2c58bceed39adde
UmamahaswariV <136911617+UmamahaswariV@users.noreply.github.com>
2026-02-15 23:35:02 +0530

    java Challenge 5

---
+++ JavaChallenge/HappyNumbers.java
@@ -0,0 +1,61 @@
+/*
+Java Challenge (5/20)
+Write an algorithm to determine if a number n is happy.
+A happy number is a number defined by the following process:
+Starting with any positive integer, replace the number by the sum of the squares of its digits.
+Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+Those numbers for which this process ends in 1 are happy.
+Return true if n is a happy number, and false if not.
+Example 1:
+Input: n = 19
+Output: true
+Explanation:
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+Example 2:
+Input: n = 2
+Output: false
+*/
+
+import java.util.Scanner;
+public class HappyNumbers {
+	public boolean isHappy(int n) {
+        while (n != 1 && n != 4) {
+            n = getSumOfSquares(n);
+        }
+        return n == 1;
+    }
+    
+    private int getSumOfSquares(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+    	System.out.println("Enter the Number for Happy Number Algorithm");
+ 		int input=sc.nextInt();
+ 		HappyNumbers Hn=new HappyNumbers();
+ 		boolean result = Hn.isHappy(input);
+ 		if(result) {
+ 			System.out.println("True");
+ 			System.out.println(input+" ==> HAPPY");
+ 		}
+ 		else {
+ 			System.out.println("False");
+ 			System.out.println(input+" ==> UNHAPPY");
+ 		}
+ 		sc.close();
+	}
+}
+
+
+
+  
